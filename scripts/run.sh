d556ee2c35da4b1f561fbc125ed1dc1d91e7aee0
Daniel Suo <danielsuo@gmail.com>
2020-08-08 07:10:52 -0400

    Update data

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 pushd $DIR/..
 git pull
-/home/dsuo/miniconda3/envs/provid/bin/python $DIR/../provid/source/__init__.py
+python $DIR/../provid/source/__init__.py
 git commit -am "Update data"
 git push
 popd
